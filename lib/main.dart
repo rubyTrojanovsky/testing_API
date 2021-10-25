bcfe4336eba661983e83830f5b4a8140989e49d3
Ruby Trojanovsky <armymadesu@gmail.com>
2021-10-25 13:50:31 +0700

    kodingan sebelum diganti jsonnya

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
-import 'dart:convert'; 
-import 'second.dart';// for using json.decode()
+import 'dart:convert';
+// import 'Article.dart'; // for using json.decode()
 
 void main() {
   runApp(const MyApp());
@@ -38,29 +38,84 @@ class _HomePageState extends State<HomePage> {
   }
 
   // The list that contains information about photos
-  List _loadedPhotos = [];
+  List _articleData = [];
 
   // The function that fetches data from the API
   Future<void> _fetchData() async {
-
-    final response = await http.get(Uri.parse('https://web-kg2.herokuapp.com/api/artikel'));
-    final data = json.decode(response.body);
+    final response =
+        await http.get(Uri.parse('https://web-kg2.herokuapp.com/api/artikel'));
+    final data = jsonDecode(response.body);
 
     setState(() {
-      _loadedPhotos = data;
+      _articleData = data;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: Scaffold(
+        home: Scaffold(
+            appBar: AppBar(
+                backgroundColor: const Color(0xFF2995B2),
+                leading: GestureDetector(
+                    onTap: () {
+                      print("asd");
+                    },
+                    child: const Icon(Icons.arrow_back_ios)),
+                actions: <Widget>[
+                  Padding(
+                      padding: const EdgeInsets.only(right: 20.0),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: const Icon(
+                          Icons.more_vert,
+                        ),
+                      )),
+                ]),
+            body: SafeArea(
+                child: ListView.builder(
+              itemCount: _articleData.length,
+              itemBuilder: (BuildContext ctx, index) {
+                return Card(
+                    elevation: 5.0,
+                    margin: const EdgeInsets.all(20),
+                    child: ListTile(
+                      // leading: Container(
+                      //   color: Colors.blue,
+                      //   width: 150,
+                      // ),
+                      leading: Image.network(
+                        _articleData[index]["gambar"],
+                        
+                        fit: BoxFit.cover,
+                      ),
+                      title: Text(_articleData[index]['judul']),
+                      subtitle: Text(_articleData[index]["deskripsi"]),
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Article(judul: _articleData[index]['judul'], isiArtikel: _articleData[index]["deskripsi"],)));
+                      },
+                    ));
+              },
+            ))));
+  }
+}
+
+
+class Article extends StatelessWidget{
+ final String judul;
+ final String isiArtikel;
+
+  const Article({Key? key, required this.judul, required this.isiArtikel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           backgroundColor: const Color(0xFF2995B2),
           leading: GestureDetector(
           onTap: (){
-            print("asd");
+            Navigator.pop(context);
           },
           child: const Icon(Icons.arrow_back_ios)
         ),
@@ -75,31 +130,28 @@ class _HomePageState extends State<HomePage> {
       )
     ),]
       ),
-      
-        body: SafeArea(
-            child:ListView.builder(
-                    itemCount: _loadedPhotos.length,
-                    itemBuilder: (BuildContext ctx, index) {
-                      return ListTile(
-                        leading: Container(
-                          color: Colors.blue,
-                          width: 150,
-                          
-                        ),
-                        // leading: Image.network(
-                        //   _loadedPhotos[index]["gambar"],
-                        //   width: 150,
-                        //   fit: BoxFit.cover,
-                        // ),
-                        title: Text(_loadedPhotos[index]['judul']),
-                        subtitle:
-                            Text(_loadedPhotos[index]["deskripsi"]),
-                        onTap: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => Second()));
-                        },
-                      );
-                    },
-                  ))));
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Text(
+                  judul,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                isiArtikel,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+    
   }
 }
 
