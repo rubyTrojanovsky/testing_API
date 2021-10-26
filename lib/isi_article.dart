a38449ee80ac03db674d1e948e5ef0ae9de461a8
Ruby Trojanovsky <armymadesu@gmail.com>
2021-10-26 19:23:45 +0700

    pembagian

---
+++ lib/isi_article.dart
@@ -0,0 +1,57 @@
+import 'articlelist.dart';
+import 'model_artikel.dart';
+import 'package:flutter/material.dart';
+
+
+class IsiArticle extends StatelessWidget{
+ final String judul;
+ final String isiArtikel;
+
+  const IsiArticle({Key? key, required this.judul, required this.isiArtikel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF2995B2),
+          leading: GestureDetector(
+          onTap: (){
+            Navigator.pop(context);
+          },
+          child: const Icon(Icons.arrow_back_ios)
+        ),
+        actions: <Widget>[
+    Padding(
+      padding: const EdgeInsets.only(right: 20.0),
+      child: GestureDetector(
+        onTap: () {},
+        child: const Icon(
+            Icons.more_vert,
+          ),
+      )
+    ),]
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Text(
+                  judul,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                isiArtikel,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+    
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:async';
-import 'dart:convert';
+import 'articlelist.dart';
+
 // import 'Article.dart'; // for using json.decode()
 
 void main() {
@@ -17,141 +16,10 @@ class MyApp extends StatelessWidget {
       // Hide the debug banner
       debugShowCheckedModeBanner: false,
       title: 'Testing',
-      home: HomePage(),
+      home: ArticleList(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  //inisiasi data
-  @override
-  void initState() {
-    _fetchData();
-    super.initState();
-  }
-
-  // The list that contains information about photos
-  List _articleData = [];
-
-  // The function that fetches data from the API
-  Future<void> _fetchData() async {
-    final response =
-        await http.get(Uri.parse('https://web-kg2.herokuapp.com/api/artikel'));
-    final data = jsonDecode(response.body);
-
-    setState(() {
-      _articleData = data;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-                backgroundColor: const Color(0xFF2995B2),
-                leading: GestureDetector(
-                    onTap: () {
-                      print("asd");
-                    },
-                    child: const Icon(Icons.arrow_back_ios)),
-                actions: <Widget>[
-                  Padding(
-                      padding: const EdgeInsets.only(right: 20.0),
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: const Icon(
-                          Icons.more_vert,
-                        ),
-                      )),
-                ]),
-            body: SafeArea(
-                child: ListView.builder(
-              itemCount: _articleData.length,
-              itemBuilder: (BuildContext ctx, index) {
-                return Card(
-                    elevation: 5.0,
-                    margin: const EdgeInsets.all(20),
-                    child: ListTile(
-                      // leading: Container(
-                      //   color: Colors.blue,
-                      //   width: 150,
-                      // ),
-                      leading: Image.network(
-                        _articleData[index]["gambar"],
-                        
-                        fit: BoxFit.cover,
-                      ),
-                      title: Text(_articleData[index]['judul']),
-                      subtitle: Text(_articleData[index]["deskripsi"]),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Article(judul: _articleData[index]['judul'], isiArtikel: _articleData[index]["deskripsi"],)));
-                      },
-                    ));
-              },
-            ))));
-  }
-}
-
-class Article extends StatelessWidget{
- final String judul;
- final String isiArtikel;
-
-  const Article({Key? key, required this.judul, required this.isiArtikel}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF2995B2),
-          leading: GestureDetector(
-          onTap: (){
-            Navigator.pop(context);
-          },
-          child: const Icon(Icons.arrow_back_ios)
-        ),
-        actions: <Widget>[
-    Padding(
-      padding: const EdgeInsets.only(right: 20.0),
-      child: GestureDetector(
-        onTap: () {},
-        child: const Icon(
-            Icons.more_vert,
-          ),
-      )
-    ),]
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Text(
-                  judul,
-                ),
-              ),
-            ),
-            Center(
-              child: Text(
-                isiArtikel,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-    
-  }
-}
 
 ///TODO: flutter run -d chrome --web-renderer html
