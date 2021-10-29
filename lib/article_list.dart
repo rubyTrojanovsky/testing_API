0d27e0178a0649150f51b617c51eba8e91899ac8
Ruby Trojanovsky <armymadesu@gmail.com>
2021-10-29 10:51:14 +0700

    add gambar di list tile

--- lib/article_list.dart
+++ lib/article_list.dart
@@ -6,7 +6,7 @@ import 'dart:convert';
 import 'model_artikel.dart';
 
 class ArticleList extends StatelessWidget {
-  const ArticleList({Key? key}) : super(key: key);
+  const ArticleList({Key? key}) : super(key: key);  
 
   @override
   Widget build(BuildContext context) {
@@ -34,41 +34,58 @@ class ArticleList extends StatelessWidget {
             future: _fetchData(),
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return ListView.builder(
-                    itemExtent: 120,
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        elevation: 5.0,
-                        margin: const EdgeInsets.all(10),
-                        child: ListTile(
-                          // leading: Image.network(
-                          //   snapshot.data![index].gambar,
-                          //   width: 100,
-                          //   fit: BoxFit.cover,
-                          // ),
-                          title: Text(snapshot.data![index].judul,
-                          style: const TextStyle(fontSize: 18),),
-                          subtitle: Text(snapshot.data![index].deskripsi),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => IsiArticle(
-                                          judul: snapshot.data![index].judul,
-                                          isiArtikel:
-                                              snapshot.data![index].deskripsi,
-                                          gambar:
-                                            snapshot.data![index].gambar,
-                                        )));
-                          },
-                        ),
-                      );
-                    });
+                return(ListView.builder(
+                      itemExtent: 120,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap:  () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => IsiArticle(
+                                              judul: snapshot.data![index].judul,
+                                              isiArtikel:
+                                                  snapshot.data![index].deskripsi,
+                                              gambar:
+                                                snapshot.data![index].gambar,
+                                            )));
+                              },
+                            child: Card(
+                              elevation: 5.0,
+                              margin: const EdgeInsets.all(10),
+                              child: Row(
+                                children: [
+                                  Image.network(snapshot.data![index].gambar, width: 125,height: 100, fit: BoxFit.cover,),
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                                          child: Text(snapshot.data![index].judul,
+                                          style: const TextStyle(fontSize: 20),),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(bottom: 6.0, left: 12.0, right: 12.0 ),
+                                          child: Text(snapshot.data![index].deskripsi,
+                                          maxLines: 3,
+                                          overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                        ));
+                      })
+                );
               } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
+                return const Center(child: Text("Terjadi masalah saat menghubungkan ke server, coba ulangi lagi"));
               }
-              return const CircularProgressIndicator();
+              return const Center(child: CircularProgressIndicator());
             },
           ),
         ),
@@ -93,6 +110,7 @@ class ArticleList extends StatelessWidget {
       String gambarawal = data[i]['gambar'];
       String linkKG = 'https://web-kg2.herokuapp.com/';
       String gambar= (linkKG+gambarawal);
+      
 
       ArticleDataList konten =
           ArticleDataList(judul: judul, gambar: gambar, deskripsi: deskripsi);
